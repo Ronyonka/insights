323d0c4a5fbc6edbc7e399edff63e429614cb7c0
ronyonka <rononyonka@gmail.com>
2020-02-16 17:36:03 +0300

    added start up model

--- insights/settings.py
+++ insights/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,10 +77,16 @@ WSGI_APPLICATION = 'insights.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insights',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': ''
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 
 # Password validation
--- start_ups/models.py
+++ start_ups/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class StartUp(models.Model):
+    unique_code = models.CharField(max_length=25, unique=True, blank=False, null=False)
+    company_name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    linkedin = models.URLField(blank=True, null=True)
+    company_website = models.URLField(unique=True, null=False, blank=False)
+    hq_city = models.CharField(max_length=80, null=False, blank=False)
+    hq_country = models.CharField(max_length=80, null=False, blank=False)
+    founded_date = models.DateField()
+    sector = models.CharField(max_length=80, null=False, blank=True)
+    last_funding_amount = models.FloatField()
+    total_funding_amount = models.FloatField()
+
+    def __str__(self):
+        return self.company_name
+
+
