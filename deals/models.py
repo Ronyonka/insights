7e7c829ee8e1d52a1d36eb95ed4a98a3ce95627b
ronyonka <rononyonka@gmail.com>
2020-02-17 00:10:53 +0300

    added deals funding info model

--- deals/models.py
+++ deals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from ..insights.models import StartUp
 
-# Create your models here.
+class FundingInfo(models.Model):
+    start_up = models.ForeignKey(StartUp, on_delete=models.CASCADE, related_name="start_up")
+    round_number = models.IntegerField()
+    funded_date = models.DateField()
+    round_type = models.CharField(max_length=80, null=False, blank=True)
+    investor_name = models.CharField(max_length=80, null=False, blank=True)
+    lead_investor = models.BooleanField()
+    total_funding_round = models.FloatField()
+
+    
